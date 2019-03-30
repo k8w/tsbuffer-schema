ac47c7e1b23d2cc079efcae313baf2311956a688
k8w <twoeo@qq.com>
2019-03-31 00:25:30 +0800

    Enum -> id & value

--- src/schemas/EnumTypeSchema.ts
+++ src/schemas/EnumTypeSchema.ts
@@ -2,5 +2,9 @@ import BaseSchema from '../BaseSchema';
 
 export default interface EnumTypeSchema extends BaseSchema {
     type: 'Enum';
-    values: (string | number)[];
+    values: {
+        /** 序列化用 */
+        id: number,
+        value: string | number
+    }[];
 }
