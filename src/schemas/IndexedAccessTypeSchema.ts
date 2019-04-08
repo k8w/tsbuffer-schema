b0cc1c7fa0c1c71bb0e526a4c7f4f2b3189ca1c8
k8w <twoeo@qq.com>
2019-04-08 18:54:48 +0800

    new IndexedAccessType

--- src/schemas/IndexedAccessTypeSchema.ts
+++ src/schemas/IndexedAccessTypeSchema.ts
@@ -4,10 +4,20 @@ import LiteralTypeSchema from './LiteralTypeSchema';
 import UnionTypeSchema from './UnionTypeSchema';
 
 /**
- * Reference like XXX['XX']
+ * Reference like XXX['XX']['XX']
+ * 注意 A['b' | 'c']不是IndexedAccessType，它们应该作为一个 UnionType被识别
+ * 应该等价于 A['b'] | A['c']
  */
 export default interface IndexedAccessTypeSchema extends BaseSchema {
     type: 'IndexedAccess';
-    objectType: ReferenceTypeSchema;
-    index: LiteralTypeSchema | UnionTypeSchema;
+    objectType: ReferenceTypeSchema | IndexedAccessTypeSchema;
+    index: string;
 }
+
+interface A {
+    a: { a: string; }
+    b: { b: string; }
+    c: { c: string; }
+}
+
+type Test = A['a' | 'b' | 'c']
