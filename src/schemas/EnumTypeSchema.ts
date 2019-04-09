bdd995f4d209875489277eb262b09a9fb109dc2a
k8w <twoeo@qq.com>
2019-04-09 21:41:40 +0800

    Enum values -> members

--- src/schemas/EnumTypeSchema.ts
+++ src/schemas/EnumTypeSchema.ts
@@ -2,7 +2,7 @@ import BaseSchema from '../BaseSchema';
 
 export default interface EnumTypeSchema extends BaseSchema {
     type: 'Enum';
-    values: {
+    members: {
         /** 序列化用 */
         id: number,
         value: string | number
