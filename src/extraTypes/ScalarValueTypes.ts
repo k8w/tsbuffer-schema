b98caa6fd9ebfafd741d3ce241baff88d93d2584
k8w <twoeo@qq.com>
2019-04-22 11:17:27 +0800

    NumberType -> ProtoBuf

--- src/extraTypes/ScalarValueTypes.ts
+++ src/extraTypes/ScalarValueTypes.ts
@@ -1,11 +1,17 @@
 // TSBuffer extra scalar value types alias (for serialization)
-declare type int8 = number;
-declare type int16 = number;
+
+// Varint Encoding
 declare type int32 = number;
 declare type int64 = number;
-declare type uint8 = number;
-declare type uint16 = number;
 declare type uint32 = number;
 declare type uint64 = number;
-declare type float32 = number;
-declare type float64 = number;
+declare type sint32 = number;
+declare type sint64 = number;
+
+// Fixed Length Encoding
+declare type fixed32 = number;
+declare type fixed64 = number;
+declare type sfixed32 = number;
+declare type sfixed64 = number;
+declare type float = number;
+declare type double = number;
--- src/schemas/NumberTypeSchema.ts
+++ src/schemas/NumberTypeSchema.ts
@@ -3,5 +3,5 @@ import BaseSchema from '../BaseSchema';
 export default interface NumberTypeSchema extends BaseSchema {
     type: 'Number';
     /** 默认为float64 */
-    scalarType?: 'int8' | 'int16' | 'int32' | 'int64' | 'uint8' | 'uint16' | 'uint32' | 'uint64' | 'float32' | 'float64';
+    scalarType?: 'int32' | 'int64' | 'uint32' | 'uint64' | 'sint32' | 'sint64' | 'fixed32' | 'fixed64' | 'sfixed32' | 'sfixed64' | 'float' | 'double';
 }
