1b9e7782bc0c98b0f4dcd33862f5f8ead4784ca4
k8w <twoeo@qq.com>
2019-05-10 18:08:20 +0800

    rem

--- src/schemas/AnyTypeSchema.ts
+++ src/schemas/AnyTypeSchema.ts
@@ -1,6 +1,6 @@
 import { BaseSchema } from '../BaseSchema';
 
-/** 除ArrayBuffer以外的所有 将通过JSON.parse来解析 */
+/** 任意类型 */
 export interface AnyTypeSchema extends BaseSchema {
     type: 'Any';
 }
--- src/schemas/NumberTypeSchema.ts
+++ src/schemas/NumberTypeSchema.ts
@@ -2,6 +2,10 @@ import { BaseSchema } from '../BaseSchema';
 
 export interface NumberTypeSchema extends BaseSchema {
     type: 'Number';
-    /** 默认为float64 */
+    /** 
+     * 默认为float64
+     * bigint 默认对应uint64
+     * 只有 x64 允许bigint
+     */
     scalarType?: 'int32' | 'int64' | 'uint32' | 'uint64' | 'sint32' | 'sint64' | 'fixed32' | 'fixed64' | 'sfixed32' | 'sfixed64' | 'float' | 'double';
 }
