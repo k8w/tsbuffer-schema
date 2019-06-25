bf09e2d3ef75e9947889b66c416991e0ffe97b0d
k8w <twoeo@qq.com>
2019-06-25 12:17:41 +0800

    import extraTypes

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { TSBufferProto } from './src/TSBufferProto';
 import { TSBufferSchema } from './src/TSBufferSchema';
+import './src/extraTypes/index';
 
 export { TSBufferSchema };
 export { TSBufferProto };
