64666213a1e07b724536fbb630a104d970cdc5ba
k8w <me@k8w.io>
2019-11-15 20:14:42 +0800

    1.0.0 移除32位类型

--- src/extraTypes/ScalarValueTypes.ts
+++ src/extraTypes/ScalarValueTypes.ts
@@ -5,9 +5,6 @@ declare type int = number;
 declare type uint = number;
 
 // Fixed Length Encoding
-declare type int32 = number;
-declare type uint32 = number;
-declare type float = number;
 declare type double = number;
 declare type bigint64 = bigint;
 declare type biguint64 = bigint;
--- src/schemas/NumberTypeSchema.ts
+++ src/schemas/NumberTypeSchema.ts
@@ -7,5 +7,5 @@ export interface NumberTypeSchema extends BaseSchema {
      * bigint 默认对应uint64
      * 只有 x64 允许bigint
      */
-    scalarType?: 'int' | 'uint' | 'bigint' | 'int32' | 'uint32' | 'bigint64' | 'biguint64' | 'float' | 'double';
+    scalarType?: 'int' | 'uint' | 'bigint' | 'bigint64' | 'biguint64' | 'double';
 }
