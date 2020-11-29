55e266368d32dc5cc71e631ff40100df31c24ec9
k8w <me@k8w.io>
2020-11-29 20:59:32 +0800

    1.2.1

--- src/schemas/OverwriteTypeSchema.ts
+++ src/schemas/OverwriteTypeSchema.ts
@@ -1,11 +1,9 @@
 import { InterfaceTypeSchema } from './InterfaceTypeSchema';
 import { InterfaceReference } from '../InterfaceReference';
-import { IntersectionTypeSchema } from './IntersectionTypeSchema';
-import { UnionTypeSchema } from './UnionTypeSchema';
 
 /** equivalent to pick & overwrite */
 export interface OverwriteTypeSchema {
     type: 'Overwrite';
     target: InterfaceTypeSchema | InterfaceReference;
-    overwrite: InterfaceTypeSchema | InterfaceReference | UnionTypeSchema | IntersectionTypeSchema;
+    overwrite: InterfaceTypeSchema | InterfaceReference;
 }
