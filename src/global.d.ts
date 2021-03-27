3498e3af81f3c1cb4384a135eea7deecbdf080a0
k8w <me@k8w.io>
2021-03-27 16:33:04 +0800

    global scalar

---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+// Scalar Types
+// Varint Encoding
+export type int = number;
+export type uint = number;
+// Fixed Length (64bits) Encoding
+export type double = number;
+export type bigint64 = bigint;
+export type biguint64 = bigint;
--- src/schemas/NumberTypeSchema.ts
+++ src/schemas/NumberTypeSchema.ts
@@ -7,14 +7,3 @@ export interface NumberTypeSchema {
      */
     scalarType?: 'int' | 'uint' | 'bigint' | 'bigint64' | 'biguint64' | 'double';
 }
-
-declare global {
-    // Varint Encoding
-    type int = number;
-    type uint = number;
-
-    // Fixed Length Encoding
-    type double = number;
-    type bigint64 = bigint;
-    type biguint64 = bigint;
-}
